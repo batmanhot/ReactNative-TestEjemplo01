99b6320e1babd44c9117398bf47ba20853c3bf4a
Jhon Ponte <batmanponte@outlook.com>
2022-11-16 02:29:38 -0500

    toda la app

--- App.js
+++ App.js
@@ -1,34 +1,97 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
+import { Text, View, TextInput, Button, FlatList, Modal, ToucheableOpacity, ActivityIndicator } from 'react-native';
+import { styles }  from './styles'
 
 export default function App() {
+  const [task, setTask] = useState('')
+  const [taskList, setTaskList] = useState([])
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedTask, setSelectedTask] = useState(null);
+
+  const onHandleTask = () =>{
+    // setTaskList([...taskList, ...task]);
+    setTaskList((prevTaskList)=> [...prevTaskList, { id: Math.random().toString(),value:task, backgroundColor : getRandomBackgroundColor() }]);
+    
+    setTask('');    
+  }
+
+  const renderItem = ({item}) => (
+    <ToucheableOpacity style={styles.listItemContainer} onPress={()=> onHandleSelected}>
+      <Text style={styles.listItem}>
+        {item.value}
+      </Text>         
+    </ToucheableOpacity>    
+  ) 
+
+const onHandleSelected = (item) => {
+  setSelectedTask(item);
+  setSelectedVisible(!modalVisible);
+}
+
+  const onHandleCancel=()=>{
+    setModalVisible(!modalVisible)   
+  }
+
+  const onHandleDeleted=()=>{
+    setTaskList((prevTaskList) => prevTaskList.filter((item) => item.id === selectedTask.id ))    
+    setModalVisible(!modalVisible) 
+  }
+
+  const getRandomBackgroundColor = () =>{
+    const colors = ['blue', 'red','#212121'];
+  return colors = [Math.floor(Math.random() * colors.length)]}
+  
   return (
-    <View style={styles.container}>      
-       <View style={styles.inputContainer}>
-          <TextInput style={styles.input} />
-          <Button title='Agregar' color='#9A848F' onPress={() => null} />
+
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput 
+            style={styles.input}
+            value={task}
+            placeholder={'Ingrese Tarea'}
+            onChangeText = {text => setTask(text)}            
+            />
+          <Button disabled ={!task} title='Agregar' color='#9A848F' onPress={onHandleTask}  />
+        </View>
+
+        
+        <View style={styles.listContainer}>          
+           <Text style={styles.listTitle}>Todo List</Text> 
+
+           {/* --- Ejemplo del animacion de espera */}
+
+           <ActivityIndicator size="large" color="#94849F" />   
+
+            <FlatList
+              style={styles.listContainer}
+              data={taskList}
+              renderItem={renderItem}
+              keyExtractor={item => item.id}
+            />
+
+            <Modal visible={modalVisible} animationType='slide'>
+              <View style={styles.modalContainer}>
+                 <Text style={styles.modalTitle}>Task Detail</Text>
+              </View>
+              <View style={styles.modalDetailContainer}>
+                 <Text style={styles.modalDetailText}>Are you sure </Text>
+                 <Text style={styles.selectedTask}>Are you sure </Text>
+              </View>
+              <View style={styles.modalButtonContainer}>
+                <Button
+                  title='Cancel'
+                  color={'#A9A848F'}
+                  onPress = {onHandleCancel}
+                />
+                <Button
+                  title='Delete'
+                  onPress = {onHandleDeleted}
+                />
+              </View>
+
+            </Modal>
         </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  inputContainer:{
-    flexDirection :'row' ,
-    justifyContent:  'space-between',
-    marginHorizontal: 20,
-    marginVertical: 40,    
-  },
-  input:{
-    width: '80%',
-    borderBotomWidth:1,
-    borderBotomColor: '#9A848F'
-  }
-
-});
---
+++ styles.js
@@ -0,0 +1,89 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({    
+    container: {
+      flex: 1,
+      backgroundColor: '#fff'
+    },
+    inputContainer:{
+      flexDirection :'row' ,
+      justifyContent:  'space-between',
+      marginHorizontal: 20,
+      marginVertical: 40
+    },
+    input:{
+      width: '80%',
+      borderBotomWidth:1,
+      borderBotomColor: '#9A848F'
+    },
+    listContainer:{
+        marginHorizontal:20
+    },
+    listTitle:{
+        fontSize:18,
+        fontWeight:'bold',
+        marginBottom: '10',
+        color:'#212121'
+    },
+    listItemContainer:{            
+        paddingVertical:20,        
+        backgroundColor: '#9A848F',
+        borderRadius: 5,
+        shadowColor:'#000',
+        shadowOffset:{
+            width: 0,
+            height: 2
+         },
+         shadowOpacity : 0.25,
+         shadowRadius : 3.84,
+         elevation : 5,
+         justifyContent:'center',         
+         marginVertical:5
+    },
+
+    listItem:{
+     fontSize:14,
+     color: '#ffffff',
+     paddingHorizontal: 10
+    },
+
+
+    modalContainer : {
+      justifyContent:'center',         
+      alignItems:'center',      
+      margintop: 50,
+      paddingVertical: 20
+    },
+
+    modalDetailContainer : {
+      paddingVertical: 20,
+      justifyContent:'center',         
+      alignItems:'center',      
+    },
+
+    modalTitle:{
+      fontsize=18,
+      fontWeight='bold',
+      marginBottom=10,
+    },
+
+    modalDetailText : {
+      fontsize : 14,
+      color: '#212121',      
+    },
+    
+    selectedTask:{
+      fontsize : 14,
+      color: '#212121',
+      fontWeight:'bold',
+      paddingVertical: 20,
+    },
+
+    modalButtonContainer : {
+      width: '70%',
+      flexDirection :'row',
+      justifyContent: 'space-around', 
+      marginHorizontal:20,
+    }
+
+});
